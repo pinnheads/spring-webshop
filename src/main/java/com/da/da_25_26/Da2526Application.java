0615f293a39a3eb56065471fff799fcd46962e67
Utsav Deep <utsavdeep01@gmail.com>
2025-10-16 15:31:46 +0200

    0-Create application

---
+++ src/main/java/com/da/da_25_26/Da2526Application.java
@@ -0,0 +1,13 @@
+package com.da.da_25_26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Da2526Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Da2526Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/da/da_25_26/Da2526ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.da.da_25_26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Da2526ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
